647de0eb84445bc3355f076752df2013376d5beb
RITESH HARGE <135429163+Ritesh-Harge1@users.noreply.github.com>
2025-08-26 11:15:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
-function indexOfIgnoreCase(s1, s2) {
-  // write your code here
+function indexOfIgnoreCase(str, subStr) {
+  // Edge case: if subStr is empty, return 0
+  if (subStr.length === 0) return 0;
+
+  // Convert both strings to lowercase for case-insensitive comparison
+  let lowerStr = str.toLowerCase();
+  let lowerSub = subStr.toLowerCase();
+
+  // Use indexOf to find first occurrence
+  return lowerStr.indexOf(lowerSub);
 }
 
-// Please do not change the code below
-const s1 = prompt("Enter s1:");
-const s2 = prompt("Enter s2:");
-alert(indexOfIgnoreCase(s1, s2));
